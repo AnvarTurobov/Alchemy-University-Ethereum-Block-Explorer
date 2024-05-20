79f5e35265cfdb97dcb9bfc76ec9d9b83c9403a7
Anvar Turobov <anvar.turobov@gmail.com>
2024-05-20 16:38:27 +0100

    lifted the state up to App.js, so I can pass on the State to other components as Props, this will make sure al the coomponents use the same data consistently

--- src/App.js
+++ src/App.js
@@ -1,44 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Stats from './components/Stats';
 import BlocksAndTransactions from './components/BlocksAndTransactions';
-
+import axios from 'axios';
 import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
 
 const settings = {
-    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-    network: Network.ETH_MAINNET,
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
 };
 
 const alchemy = new Alchemy(settings);
 
-
 function App() {
-    const [blockNumber, setBlockNumber] = useState();
+  const [ethPrice, setEthPrice] = useState(null);
+  const [ethPriceChange, setEthPriceChange] = useState(null);
+  const [marketCap, setMarketCap] = useState(null);
+  const [transactionCount, setTransactionCount] = useState(null);
+  const [medGasPrice, setMedGasPrice] = useState(null);
+  const [lastFinalizedBlock, setLastFinalizedBlock] = useState(null);
+  const [lastSafeBlock, setLastSafeBlock] = useState(null);
+  const [blockNumber, setBlockNumber] = useState(); 
 
-    useEffect(() => {
-        async function getBlockNumber() {
-            setBlockNumber(await alchemy.core.getBlockNumber());
-        }
+  useEffect(() => {
+    const fetchEthData = async () => {
+      try {
+        // Getting ETH price and market cap from CoinGecko api
+        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+          params: {
+            vs_currency: 'usd',
+            ids: 'ethereum',
+          },
+        });
 
-        getBlockNumber();
-    }, []);
+        const ethData = response.data[0];
+        setEthPrice(ethData.current_price.toFixed(2));
+        setEthPriceChange(ethData.price_change_percentage_24h.toFixed(2));
+        setMarketCap(ethData.market_cap.toLocaleString());
 
+        // Getting latest Block number, gas price to get average gas price, and block from Alchemy
+        const [blockNumber, gasPrice, block] = await Promise.all([
+          alchemy.core.getBlockNumber(),
+          alchemy.core.getGasPrice(),
+          alchemy.core.getBlock('latest'),
+        ]);
 
-    return (
-        <div className="bg-gray-100 font-sans leading-normal tracking-normal">
-            <Navbar />
-            <Header />
-            <main className="container mx-auto p-8">
-                <Stats blockNumber={blockNumber}/>
-                
-                <BlocksAndTransactions />
-            </main>
-        </div>
-    );
+        setBlockNumber(blockNumber);
+        setLastFinalizedBlock(blockNumber);
+        setLastSafeBlock(blockNumber);
+        setMedGasPrice((gasPrice / 1e9).toFixed(2)); // Converting Wei to Gwei
+
+        // Estimating transaction count 
+        setTransactionCount(block.transactions.length);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchEthData();
+  }, []);
+
+  return (
+    <div className="bg-gray-100 font-sans leading-normal tracking-normal">
+      <Navbar />
+      <Header />
+      <main className="container mx-auto p-8">
+        <Stats
+          ethPrice={ethPrice}
+          ethPriceChange={ethPriceChange}
+          marketCap={marketCap}
+          transactionCount={transactionCount}
+          medGasPrice={medGasPrice}
+          lastFinalizedBlock={lastFinalizedBlock}
+          lastSafeBlock={lastSafeBlock}
+          blockNumber={blockNumber}
+        />
+        <BlocksAndTransactions lastFinalizedBlock={lastFinalizedBlock} />
+      </main>
+    </div>
+  );
 }
 
 export default App;
-
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,47 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import alchemy from '../alchemy'; 
+import React from 'react';
 
-function Stats() {
-  const [ethPrice, setEthPrice] = useState(null);
-  const [ethPriceChange, setEthPriceChange] = useState(null);
-  const [marketCap, setMarketCap] = useState(null);
-  const [medGasPrice, setMedGasPrice] = useState(null);
-  const [blockNumber, setBlockNumber] = useState(null); // TODO: will use it soon
-
-  useEffect(() => {
-    const fetchEthData = async () => {
-      try {
-        // Getting ETH price and market cap from CoinGecko
-        const response = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
-          params: {
-            vs_currency: 'usd',
-            ids: 'ethereum',
-          },
-        });
-
-        const ethData = response.data[0];
-        setEthPrice(ethData.current_price.toFixed(2));
-        setEthPriceChange(ethData.price_change_percentage_24h.toFixed(2));
-        setMarketCap(ethData.market_cap.toLocaleString());
-
-        // Getting latest Block number, gas price to get average gas price, and block from Alchemy
-        const [blockNumber, gasPrice, block] = await Promise.all([
-          alchemy.core.getBlockNumber(),
-          alchemy.core.getGasPrice(),
-          alchemy.core.getBlock('latest'),
-        ]);
-
-        setMedGasPrice((gasPrice / 1e9).toFixed(2)); // Converting Wei to Gwei
-
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchEthData();
-  }, []);
-
+function Stats({
+  ethPrice,
+  ethPriceChange,
+  marketCap,
+  medGasPrice,
+  blockNumber,
+}) {
   return (
     <div className="grid grid-cols-3 gap-4 text-center text-gray-700 mb-8">
       <div className="bg-white p-6 rounded shadow">
